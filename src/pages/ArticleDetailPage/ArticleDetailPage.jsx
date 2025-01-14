a79643c3c61391c5b8719a5d475fb0111ea788be
Antonia Frey <antonia.frey@outlook.com>
2025-01-14 15:59:54 +0100

    add button to details page

--- src/pages/ArticleDetailPage/ArticleDetailPage.jsx
+++ src/pages/ArticleDetailPage/ArticleDetailPage.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
 import articlesData from "../../db/articles.json";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../store/articleSlice";
 
 function ArticleDetailPage() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart({ id: article.id, quantity: 1 }));
+    console.log("Added to cart: ", article.name);
+  };
 
   // Get the articleId from the URL
   const { articleId } = useParams();
@@ -20,19 +28,24 @@ function ArticleDetailPage() {
   };
 
   return (
-    <Box sx={{ padding: 2}}>
+    <Box sx={{ padding: 2 }}>
+      <Button sx={{ m: 2 }} onClick={handleBackToHome}>
+        Back to home
+      </Button>
       <Typography variant="h5" sx={{ color: "red", fontWeight: "bold" }}>
         TODO: Article Details
       </Typography>
 
       {article && (
-        <Typography variant="h4" sx={{ marginY: 4 }}>
-          {article.name}
-        </Typography>
+        <Box>
+          <Typography variant="h4" sx={{ marginY: 4 }}>
+            {article.name}
+          </Typography>
+          <Button size="small" variant="contained" onClick={handleAddToCart}>
+            Add to cart
+          </Button>{" "}
+        </Box>
       )}
-      <Button sx={{ m: 2 }} onClick={handleBackToHome}>
-        Back to home
-      </Button>
     </Box>
   );
 }
